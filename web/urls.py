4e3499343f12882c1d41f445ac65c80a545a0380
Binderiya Adishaa <binderiya@Bindis-MacBook-Pro.local>
2020-12-30 03:35:04 +0800

    Update ALLOWED_HOSTS with site and development server URL

--- web/urls.py
+++ web/urls.py
@@ -17,12 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 urlpatterns += [
     path('covid_visual/', include('covid_visual.urls')),
+]
+urlpatterns += [
+    path('', RedirectView.as_view(url='covid_visual/', permanent=True)),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
